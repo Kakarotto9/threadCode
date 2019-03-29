c2e4cda2dd28a82400a00491e5563241e1dc8a9f
Kakarotto9 <18554239115@163.com>
2019-03-29 17:47:44 +0800

    1

--- lock_free_stack.h
+++ lock_free_stack.h
@@ -3,6 +3,8 @@
 #include<atomic>
 #include<memory>
 
+using std::shared_ptr;
+
 tempalte<typename T>
 class lock_free_stack{
 	struct node{
@@ -44,9 +46,14 @@ class lock_free_stack{
 		   else 
 		   old=head.load();
 		   }*/
-		while(head.exchange_and_weak(old,old->next));
-		shared_ptr<T> res(old->data);
-		delete old;
+		while(old&&  //这里用old，而不用head.load(),是为了符合pop调用的即时性
+				head.exchange_and_weak(old,old->next));
+		//shared_ptr<T> res(old->data); 这样写是错的，当old是null时，调用null指针错误
+		shared_ptr<T> res;
+		if(old){
+			res=old->data;
+			delete old;
+		}
 		return res;
 	}
-}
+};
