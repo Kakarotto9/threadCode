e7e34a2c52d26b030c46ab65ce9d4bf7087a76a4
Kakarotto9 <18554239115@163.com>
2019-04-01 21:48:41 +0800

    lock_free_stack3.h

--- lock_free_stack3.h
+++ lock_free_stack3.h
@@ -1,3 +1,9 @@
+#pragma once
+
+#include<memory>
+#include<atomic>
+
+using std::shared_ptr;
 
 template<typename T>
 class lock_free_stack
@@ -18,14 +24,22 @@ class lock_free_stack
 				data(std::make_shared<T>(data_)),
 				internal_count(0)
 			{}
-			
+
 		};
-		std::atomic<counted_node_ptr> head; // 4
+		void increase_head_count(counted_node_ptr & old){
+			// counted_node_ptr new;我本来想用 new来简洁表达，但忘了new是 关键字了
+			counted_node_ptr new_node;
+			do{
+				new_node=old;
+				++new_node.counted_node_ptr;
+			}while(head.compare_exchange_weak(old,new_node));  //4  和注释6 相互作用
+			old.external_count=new_node.external_count;
+		}
+		std::atomic<counted_node_ptr> head; 
 	public:
-		~lock_free_stack()
+		~lock_free_stack()//需要主动释放全部
 		{
-			while(pop());
-			
+			while(pop());  
 		}
 		void push(T const& data) // 5
 		{
@@ -35,12 +49,30 @@ class lock_free_stack
 			new_node.ptr->next=head.load();
 			while(!head.compare_exchange_weak(new_node.ptr->next,new_node));
 		}
-		void pop(){
+		shared_ptr<T> pop(){
 			counted_node_ptr old_node=head.load();
-			head.compare_exchange_weak(old_node,old_node.ptr->next);
-			if(old)
-
+			for(;;){  //因为判定情况，可能导致没有数据抛出，所以需要循环到数据抛出为止
+				increase_head_count(old_node);
+				node* ptr=old_node.ptr;
+				if(old_node){
+					if(head.compare_exchange_weak(old_node,ptr->next)){ // 6 
+						shared_ptr<T> res;
+						res.swap(ptr->data);
+						int count=old_node.external_count;
+						if(ptr.internal_count.fetch_add(count)==-count){
+							delete ptr;
+						}
+						return res;
+					}
+					else if(ptr->internal_count.fetch_sub()){
+						delete ptr;
+					}
+				}
+				else {
+					return shared_ptr<T> ();
+				}
+			}
 		}
-		
+
 };
 
